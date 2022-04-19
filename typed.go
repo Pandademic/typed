42fe994a8199c081834bb2d6683f3d682d1fdefd
Pandademic <atharv.gupte510@gmail.com>
2022-04-19 17:13:28 -0400

    yay! the code is back

---
+++ typed.go
@@ -0,0 +1,16 @@
+package typed
+
+import "fmt"
+// function TypeOf return a string with the type of the variable provided.
+// take's 1 arg.
+func TypeOf(s anything)string{
+     return fmt.Sprintf("%T", s)
+}
+// function IsSameType return's a boolean of wheter or not 2 variables are of the same type
+func IsSameType(first any,second any)bool{
+     if(TypeOf(first) == TypeOf(second)){
+       return true
+     }
+     return false
+
+}
